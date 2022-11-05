5cb736c64af019d32218773db54720ba18f80b13
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-11-05 23:55:29 +0100

    data fetched

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { useState, useEffect } from "react";
+import Movies from "../components/Movies";
 
 const Main = () => {
-  return <div>I am MAIN</div>;
+  const API_KEY = "34f357d9a91c3d9ccaf3fa10d802bc19";
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+
+  const [movies, setMovies] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(url);
+    const movies = await response.json();
+    setMovies(movies.results);
+
+    console.log(movies);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Main</h2>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Box>
+      <Movies movies={movies} />
+    </div>
+  );
 };
 
 export default Main;
