94a6df4b8ba0f7b9cf0fd6106a4b55370856d342
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-11-06 21:36:32 +0100

    movieDetails

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const MovieDetail = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, ad.
+      Deleniti autem excepturi error ut tempora explicabo molestias minima!
+    </div>
+  );
 };
 
 export default MovieDetail;
