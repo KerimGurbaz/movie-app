bfcc632745ec630487a22877bedb6567d600f91e
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-11-06 23:24:08 +0100

    navigate added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,88 @@
-import { Fragment } from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
-import "./navbar.styles.scss";
-const Navbar = () => {
-  return (
-    <Fragment>
-      <div className="nav-container">
-        <NavLink className="nav-logo" to="/">
-          MovieApp
-        </NavLink>
-        <div className="nav-links">
-          <NavLink className="nav-link" to="/login">
-            Login
-          </NavLink>
-          <NavLink className="nav-link" to="register">
-            Register
-          </NavLink>
-        </div>
-      </div>
-      <Outlet />
-    </Fragment>
-  );
-};
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
-export default Navbar;
+export default function Navbar() {
+  const navigate = useNavigate();
+  // const currentUser = { displayName: "kerim gurbaz" };
+  const currentUser = false;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/* <MenuIcon /> */}
+            <NavLink
+              to={"/"}
+              style={{
+                backgroundColor: "#fff",
+                width: "3rem",
+                height: "3rem",
+                borderRadius: "50%",
+              }}
+            >
+              <TheaterComedyIcon />
+            </NavLink>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React Movie APP
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {currentUser ? (
+              <>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1, m: 1, textTransform: "capitalize" }}
+                >
+                  {currentUser.displayName}
+                </Typography>
+                <NavLink
+                  to={"/login"}
+                  style={{ color: "#fff", textDecoration: "none" }}
+                >
+                  Logout
+                </NavLink>
+              </>
+            ) : (
+              <>
+                <NavLink
+                  to={"/login"}
+                  color="inherit"
+                  style={{ color: "#fff", textDecoration: "none" }}
+                >
+                  <Button color="inherit">Login</Button>
+                </NavLink>
+                <NavLink
+                  to={"/register"}
+                  style={{ color: "#fff", textDecoration: "none" }}
+                >
+                  <Button color="inherit">Register</Button>
+                </NavLink>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
